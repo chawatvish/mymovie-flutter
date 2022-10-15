529cd5d77d173951598c2106e04577c5921c717d
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2022-10-15 22:54:12 +0700

    Change bottom navigation bar decoration

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -25,14 +25,18 @@ class _MainPageState extends State<MainPage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
+  Widget _createBottomNavigationBar() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: const BoxDecoration(
+          border:
+              Border(top: BorderSide(color: Color(0xFF0296E5), width: 1.0))),
+      child: BottomNavigationBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
             label: 'Home',
           ),
           BottomNavigationBarItem(
@@ -40,14 +44,25 @@ class _MainPageState extends State<MainPage> {
             label: 'Search',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.bookmark),
+            icon: Icon(Icons.bookmark_outline),
             label: 'Watch list',
           ),
         ],
         currentIndex: _selectedIndex,
+        unselectedItemColor: const Color(0xFF67686D),
         selectedItemColor: const Color(0xFF0296E5),
+        unselectedFontSize: 15,
+        selectedFontSize: 15,
         onTap: _onItemTapped,
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: _createBottomNavigationBar(),
     );
   }
 }
